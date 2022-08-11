e8ed6b76e5bc854801ee796c3516731b43af656e
kevargas05 <77646392+kevargas05@users.noreply.github.com>
2022-08-10 20:50:54 -0600

    Create 5 - Rock Paper Scissors game.py

---
+++ 5 - Rock Paper Scissors game.py
@@ -0,0 +1,61 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+#
+import random
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+computer_choice = random.randint(0,2)
+#
+options= [rock, paper, scissors]
+print(options[player_choice])
+print("Computer chose:\n")
+print(options[computer_choice])
+#
+if player_choice == 0:
+  if computer_choice == 0:
+    print("Draw!")
+  elif computer_choice == 1:
+    print("You Lose!")
+  elif computer_choice == 2:
+    print("You Won!!!!!!")
+#
+if player_choice == 1:
+  if computer_choice == 1:
+    print("Draw!")
+  elif computer_choice == 0:
+    print("You Won!!!!!!")
+  elif computer_choice == 2:
+    print("You Lose!")
+#
+if player_choice == 2:
+  if computer_choice == 2:
+    print("Draw!")
+  elif computer_choice == 1:
+    print("You Won!!!!!!")
+  elif computer_choice == 0:
+    print("You Lose!")
+  
+
